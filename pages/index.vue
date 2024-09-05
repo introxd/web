b64f4fb4a044d05407147f9111b6e3191128a552
imba97 <mail@imba97.cn>
2024-09-05 20:58:14 +0800

    style: 版本显示位置

--- pages/index.vue
+++ pages/index.vue
@@ -10,16 +10,16 @@
     <div mt-4 text-5>
       <span>定制个人简介页面，类似</span>
       <a
-        href="/imba97"
-        ml-2 b="0 b-1 dashed black hover:#bd34fe"
-        hover="bg-clip-text text-transparent bg-gradient-to-tr"
-        from="#bd34fe" to="#47caff"
+        href="/imba97" ml-2 b="0 b-1 dashed black hover:#bd34fe"
+        hover="bg-clip-text text-transparent bg-gradient-to-tr" from="#bd34fe" to="#47caff"
       >imba97</a>
     </div>
 
-    <div mt-4 text="3 gray-3">
-      v{{ version }}
-    </div>
+    <Teleport to="body">
+      <div fixed right-2 bottom-1 text="3 gray-3">
+        v{{ version }}
+      </div>
+    </Teleport>
   </div>
 </template>
 
