e5f3fbd460520badc4d664b7329c309ef1defc0c
imba97 <mail@imba97.cn>
2024-09-06 15:21:26 +0800

    feat: iframe 加载效果

--- pages/imba97/index.vue
+++ pages/imba97/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div w-full h-full>
-    <iframe src="https://me.imba97.cn/resume" frameborder="0" h-full w-full />
+  <div relative w-full h-full>
+    <div v-show="loading" absolute top-0 left-0 h-full w-full flex items-center justify-center>
+      <div i-line-md-loading-loop h-16 w-16 bg-gray-2 />
+    </div>
+
+    <iframe v-show="!loading" ref="iframe" frameborder="0" h-full w-full animate-fade-in />
   </div>
 </template>
+
+<script lang="ts" setup>
+const iframe = ref()
+const loading = ref(true)
+
+onMounted(() => {
+  iframe.value.src = 'https://me.imba97.cn/resume'
+  iframe.value.onload = () => {
+    loading.value = false
+  }
+})
+</script>
