3fd6bd5ed9115a543188010272bd8ad2cf1c417f
imba97 <mail@imba97.cn>
2024-09-13 15:18:48 +0800

    feat: 小红书组件

---
+++ components/accounts/XiaoHongShu/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    flex items-center justify-center rounded-lg bg="#ff2442"
+    :size="props.size || '16'"
+    @click="openUrl(`xhsdiscover://user/${props.user}`)"
+  >
+    <div i-simple-icons-xiaohongshu size="80%" bg-white />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  user: string
+  size?: string
+}>()
+
+function openUrl(url: string) {
+  window.open(url, '_self')
+}
+</script>
