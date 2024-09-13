b500c2fca21d1d521dd04930f7acea890375e8d6
imba97 <mail@imba97.cn>
2024-09-13 13:07:51 +0800

    perf: 页面自适应优化

--- pages/benshsuren/components/Title.vue
+++ pages/benshsuren/components/Title.vue
@@ -2,28 +2,28 @@
   <div>
     <div flex items-center justify-between>
       <div v-if="hasImage && props.reverse">
-        <img :src="image" size-32 rounded-full b="4 white solid">
+        <img :src="image" inline lt-xs="hidden" size="lt-md:20 md:32" rounded-full b="4 white solid">
       </div>
 
       <div flex items-center gap-4>
-        <div font-italic text="7 #638167">
-          <span relative before="content-empty absolute top-[-10%] right--1 w-1px h-120% rotate-10 bg-[#638167]">
+        <div font-italic text="lt-md:7 md:8 #638167">
+          <span relative before="content-empty absolute top-[-10%] right--2 lt-md:w-1px md:w-2px h-120% rotate-10 bg-[#638167]">
             {{ props.prefix }}
           </span>
         </div>
 
         <div flex="~ col">
-          <div text="6 #71706a">
+          <div text="lt-md:5 md:6 #71706a">
             {{ props.title }}
           </div>
-          <div tracking-3 text="#638167">
+          <div tracking="lt-md:2 md:3" text="#638167">
             {{ props.subtitle }}
           </div>
         </div>
       </div>
 
       <div v-if="hasImage && !props.reverse">
-        <img :src="image" size-32 rounded-full b="4 white solid">
+        <img :src="image" inline lt-xs="hidden" size="lt-md:20 md:32" rounded-full b="4 white solid">
       </div>
     </div>
     <div mt-4>
--- pages/benshsuren/index.vue
+++ pages/benshsuren/index.vue
@@ -23,14 +23,14 @@
 
 <template>
   <div h-full w-full of-y-auto bg="#f4eddf">
-    <div v-show="!allReady" absolute ___ top-0 left-0 h-full w-full flex items-center justify-center>
+    <div v-show="!allReady" absolute top-0 left-0 h-full w-full flex items-center justify-center>
       <div fixed text-center font-linhai op-0 select-none>
         loading
       </div>
       <div i-line-md-loading-loop h-16 w-16 bg="#305f6f" />
     </div>
 
-    <div v-show="allReady" relative prose ma p-4>
+    <div v-show="allReady" relative prose ma py-4 px="lt-md:8 md:4">
       <!-- 定位元素 -->
       <div absolute top-20 right="lt-md:8 md:14" text="lt-md:8 md:10 #ede0cd" font-italic animate-fade-in>
         <div>AN HAO</div>
@@ -38,8 +38,8 @@
       </div>
 
       <div
-        v-show="currentIndex >= 2" absolute top-55 left="lt-md:60% md:50%" translate-x="-50%" font-linhai
-        text="lt-md:8 md:10 #305f6f" animate-fade-in
+        v-show="currentIndex >= 2" absolute top-55 left="lt-md:60% md:55%" translate-x="-50%" font-linhai
+        text="lt-xs:6 lt-md:10 md:12 #305f6f" animate-fade-in
       >
         <div>安好</div>
         <div>手绘</div>
