5746fe822b2267e2f6f4d3b03943760f2d7fb012
imba97 <mail@imba97.cn>
2024-09-13 17:21:32 +0800

    fix: 字体不显示

--- pages/benshsuren/Wash.vue
+++ pages/benshsuren/Wash.vue
@@ -4,7 +4,9 @@
       <div flex gap-2>
         <Heart />
         <div flex-1 text="#638167">
-          <span>手绘颜料为丝绸纺织颜料 + 纺织颜料 + 丙烯混用</span>
+          <span>手绘颜料为丝绸纺织颜料 + 纺织颜料 + 丙</span>
+          <span font-sans>烯</span>
+          <span>混用</span>
           <span>不能长时间浸泡和反复搓洗及高温熨烫，无其他注意事项，根据衣服底布护理就好</span>
         </div>
       </div>
