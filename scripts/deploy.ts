11d60f68cc11052c855097ac976c3b40793fcbac
imba97 <mail@imba97.cn>
2024-09-21 17:22:33 +0800

    ci: print token

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,4 +3,4 @@ import process from 'node:process'
 console.log('NUXT_HUB_PROJECT_KEY', process.env.NUXT_HUB_PROJECT_KEY?.length)
 console.log('NUXT_HUB_USER_TOKEN', process.env.NUXT_HUB_USER_TOKEN?.length)
 
-console.log('NUXT_HUB_USER_TOKEN', process.env.NUXT_HUB_PROJECT_KEY?.slice(-10))
+console.log('NUXT_HUB_USER_TOKEN', process.env.NUXT_HUB_USER_TOKEN?.slice(-10))
