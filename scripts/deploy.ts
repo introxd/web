14c953b74293410595c74d538cd25370e5690393
imba97 <mail@imba97.cn>
2024-09-21 16:25:01 +0800

    fix: stdout

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,12 @@
 import process from 'node:process'
 import { spawn } from 'node:child_process'
 
-spawn('npx', ['nuxthub', 'deploy', '--no-build'], {
-  stdio: 'inherit',
-
+const child = spawn('npx', ['nuxthub', 'deploy', '--no-build'], {
   env: {
     CLOUDFLARE_MODE: 'true',
     NUXT_HUB_PROJECT_KEY: process.env.NUXT_HUB_PROJECT_KEY,
     NUXT_HUB_USER_TOKEN: process.env.NUXT_HUB_USER_TOKEN
   }
 })
+
+child.stdout.pipe(process.stdout)
