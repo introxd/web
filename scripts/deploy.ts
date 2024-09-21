9a9f488c65af43660ab27c3074d842725790c69b
imba97 <mail@imba97.cn>
2024-09-21 23:31:26 +0800

    ci: update release workflow

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,11 @@
 import process from 'node:process'
 
-console.log('NUXT_HUB_PROJECT_KEY', process.env.NUXT_HUB_PROJECT_KEY?.length)
-console.log('NUXT_HUB_USER_TOKEN', process.env.NUXT_HUB_USER_TOKEN?.length)
+import { exec } from 'node:child_process'
 
+const [, , NUXT_HUB_PROJECT_KEY, NUXT_HUB_USER_TOKEN] = process.argv
+
+console.log('NUXT_HUB_PROJECT_KEY', NUXT_HUB_PROJECT_KEY.length)
+console.log('NUXT_HUB_PROJECT_KEY', NUXT_HUB_USER_TOKEN.length)
 console.log('NUXT_HUB_USER_TOKEN', process.env.NUXT_HUB_USER_TOKEN?.slice(-10))
+
+exec(`cross-env NUXT_HUB_PROJECT_KEY="${NUXT_HUB_PROJECT_KEY}" cross-env NUXT_HUB_USER_TOKEN="${NUXT_HUB_USER_TOKEN}" npx nuxthub deploy --no-build`)
