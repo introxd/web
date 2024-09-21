55568df20f2fb18127c8bea31ec5fda14e5e6115
imba97 <mail@imba97.cn>
2024-09-21 16:41:03 +0800

    ci: update release

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,17 @@
 import process from 'node:process'
+
 import { spawn } from 'node:child_process'
 
-const child = spawn('npx', ['nuxthub', 'deploy', '--no-build'], {
+const [, , NUXT_HUB_PROJECT_KEY, NUXT_HUB_USER_TOKEN] = process.argv
+
+spawn('npx', ['nuxthub', 'deploy', '--no-build'], {
   shell: true,
+  stdio: 'inherit',
+
   env: {
     ...process.env,
     CLOUDFLARE_MODE: 'true',
-    NUXT_HUB_PROJECT_KEY: process.env.NUXT_HUB_PROJECT_KEY,
-    NUXT_HUB_USER_TOKEN: process.env.NUXT_HUB_USER_TOKEN
+    NUXT_HUB_PROJECT_KEY,
+    NUXT_HUB_USER_TOKEN
   }
 })
-
-child.stdout.pipe(process.stdout)
