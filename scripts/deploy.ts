84c0a6a2451afade3b4aee9f2bedb251124653b3
imba97 <mail@imba97.cn>
2024-09-21 16:38:05 +0800

    ci: fix error

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,15 +2,13 @@ import process from 'node:process'
 import { spawn } from 'node:child_process'
 
 const child = spawn('npx', ['nuxthub', 'deploy', '--no-build'], {
+  shell: true,
   env: {
+    ...process.env,
     CLOUDFLARE_MODE: 'true',
     NUXT_HUB_PROJECT_KEY: process.env.NUXT_HUB_PROJECT_KEY,
     NUXT_HUB_USER_TOKEN: process.env.NUXT_HUB_USER_TOKEN
   }
-})
-
-child.on('error', (error) => {
-  console.log(error)
 })
 
 child.stdout.pipe(process.stdout)
