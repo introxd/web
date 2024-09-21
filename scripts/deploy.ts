7537e49aa25d6247fd993c8b7a3e418d64cd2d87
imba97 <mail@imba97.cn>
2024-09-21 16:07:41 +0800

    feat: 部署 NuxtHub 脚本

---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+import process from 'node:process'
+
+import { spawn } from 'node:child_process'
+
+const [, , NUXT_HUB_PROJECT_KEY, NUXT_HUB_USER_TOKEN] = process.argv
+
+spawn('npx', ['nuxthub', 'deploy', '--no-build'], {
+  shell: true,
+  stdio: 'inherit',
+
+  env: {
+    CLOUDFLARE_MODE: 'true',
+    NUXT_HUB_PROJECT_KEY,
+    NUXT_HUB_USER_TOKEN
+  }
+})
