53f0d13398dd3fc56c28344e4600fce51814107a
imba97 <mail@imba97.cn>
2024-09-23 11:22:43 +0800

    ci: test code

--- scripts/data.mjs
+++ scripts/data.mjs
@@ -11,7 +11,7 @@ export const $api = ofetch.create({
   onRequest({ options }) {
     options.headers = options.headers || {}
     console.log('token')
-    console.log('loadUserConfig().hub?.userToken', loadUserConfig().hub?.userToken)
+    // console.log('loadUserConfig().hub?.userToken', loadUserConfig().hub?.userToken)
     console.log('process.env.NUXT_HUB_USER_TOKEN', process.env.NUXT_HUB_USER_TOKEN)
     if (!options.headers.Authorization) {
       options.headers.Authorization = `Bearer ${loadUserConfig().hub?.userToken || process.env.NUXT_HUB_USER_TOKEN || ''}`
