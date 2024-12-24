0904a846869fdc3bdd557fe065ee02a4e48f5e32
imba97 <mail@imba97.cn>
2024-12-25 01:02:04 +0800

    feat: add chukuang page

---
+++ pages/chukuang/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div size-screen fccc gap-8 of-hidden bg-dark-6>
+    <IxFlowingLight :mask="ding" h-72 w-48 />
+    <div text="3 dark-1">
+      一个丁老头
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ding from '@/assets/images/chukuang/ding.png'
+</script>
